e24dd4be78c3d0fe9d6954f43fcb8398701f578d
Patryk Komar <kontakt.patryk.komar@gmail.com>
2018-09-01 22:15:03 +0200

    Landing page v. 0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPage } from './pages/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: LandingPage }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Logo } from './components/logo/logo.component';
+import { LandingPage } from './pages/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Logo,
+    LandingPage
   ],
   imports: [
     BrowserModule,
