49fb07ef14d67b51ca7a807c8e4704e178d055ae
Patryk Komar <kontakt.patryk.komar@gmail.com>
2018-09-07 10:54:19 +0200

    Landing page v. 0.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPage } from './pages/landing/landing.component';
+import { HomePage } from './pages/home/home.component';
+import { RegisterPage } from './pages/register/register.component';
 
 const routes: Routes = [
-  { path: "", component: LandingPage }
+  { path: "", component: LandingPage },
+  { path: "home", component: HomePage },
+  { path: "register", component: RegisterPage }
 ];
 
 @NgModule({
